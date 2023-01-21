84397f156a19581c8defaa0627eebab090083544
kai <3470837+TheLeanMachine@users.noreply.github.com>
2023-01-21 02:58:00 +0100

    VUE, part 1

--- src/main/drs-client.ts
+++ src/main/drs-client.ts
@@ -1,5 +1,14 @@
+import { createApp } from 'vue';
+
+//import { App } from 'vue';
+
 import { Dwarf } from './domain/entity/Dwarf.js'
 
 let gimli = new Dwarf('Gimli Gloinson', 42);
 
 console.log("Fuck yalll!");
+
+//const app = createApp(App)
+const app = createApp({});
+
+app.mount();
--- src/main/drs-server.js
+++
@@ -1,6 +0,0 @@
-export default {
-  port: 3000,
-  fetch(request) {
-    return new Response('<!DOCTYPE html><html><head><title>Dwarven Rogue Stronghold</title><body><script src="./dist/drs-client.js"></script></body></html>');
-  },
-};
